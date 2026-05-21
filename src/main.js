0d575fe5586d096bfa040f3146601cb4b8559d11
Nyx4788 <nicociuranaduque@gmail.com>
2026-05-21 18:31:51 +0200

    Menu i pausa fet

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import './styles/main.css'
 import Phaser from 'phaser'
+import {MenuScene} from './scenes/MenuScene.js'
 import {PlayScene} from './scenes/PlayScene.js'
+import {ExitScene} from './scenes/ExitScene.js'
+import {PauseScene} from './scenes/PauseScene.js'
 
 const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   parent: 'gameCanvas',
-  scene: [PlayScene],
+  scene: [MenuScene, PlayScene, ExitScene, PauseScene],
   physics: {
     default: 'arcade',
     arcade: {
